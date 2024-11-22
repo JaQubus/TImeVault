681684af554d1d49f4f60f37555f9b2b3fc8442f
Jakub <jakubkulik20@gmail.com>
2024-11-22 20:32:31 +0100

    Added user model and SIMPle database

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from sqlalchemy.ext.asyncio.session import async_sessionmaker
+from starlette.responses import JSONResponse
 import uvicorn
-from fastapi import FastAPI
-from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI, Request, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 import models
+from sqlalchemy import select, and_
+import hashlib
 
 app = FastAPI()
 # bro did NOT cook anything
@@ -10,7 +13,45 @@ app = FastAPI()
 Session = async_sessionmaker(bind=models.engine)
 session = Session()
 
-app.mount("/", app)
+@app.post("/login")
+async def login(request: Request, db: AsyncSession = Depends(models.get_db)) -> JSONResponse:
+    try:
+        data = await request.json()
+        print(data)
+        async with db.begin():
+            # print("1")
+            query = await db.execute(select(models.User).
+            where(
+                and_(
+                    models.User.email == data["email"],
+                    models.User.password == await hash_password(data["password"])
+                )
+            )
+            )
+            print(query)
+
+            user_data = query.scalar()
+            user_id = user_data.user_id
+
+        if user_data is None:
+            return JSONResponse(content={"Error": "User not found"}, status_code=404)
+
+        # user_id = user_data["user_id"]
+        # print("\n\n\n\n\n", user_id)
+
+        return JSONResponse(content={"Success": "User logged in successfully", "user_id": user_id}, status_code=200)
+
+    except Exception as e:
+        print(e)
+        return JSONResponse(content={"Error": f"Unexpected Error {e}"}, status_code=500)
+
+
+@app.get("/send_email")
+
+async def hash_password(password):
+    password_bytes = password.encode('utf-8')
+    hash_object = hashlib.sha256(password_bytes)
+    return hash_object.hexdigest()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8000)
--- models.py
+++ models.py
@@ -1,5 +1,13 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 import uuid
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from sqlalchemy import String
+from pydantic import BaseModel, EmailStr
+
+
+from sqlalchemy_utils import UUIDType
+
+Base = declarative_base()
 
 engine = create_async_engine('sqlite+aiosqlite:///sqlalchemy.sqlite', echo=False)
 SessionLocal = async_sessionmaker(engine)
@@ -7,3 +15,37 @@ SessionLocal = async_sessionmaker(engine)
 
 def generate_uuid():
     return int(uuid.uuid4())
+
+async def get_db() -> AsyncSession:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    await SessionLocal().close()
+    return SessionLocal()
+
+class UserCreate(BaseModel):
+    user_id: uuid.UUID
+    email: EmailStr
+    username: str
+    password: str
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id: Mapped[uuid.UUID] = mapped_column("user_id", UUIDType(binary=False), primary_key=True, index=True)
+
+    email: Mapped[String] = mapped_column("email", String, nullable=False, unique=True)
+    username: Mapped[String] = mapped_column("username", String, nullable=False)
+    password: Mapped[String] = mapped_column("password", String, nullable=False)
+
+    def __init__(self, user_id, email, username, password, sex, image):
+        self.user_id = user_id
+        self.email = email
+        self.username = username
+        self.password = password
+
+    def dict(self):
+        return {
+            "id": self.user_id,
+            "email": self.email,
+            "name": self.username,
+        }
