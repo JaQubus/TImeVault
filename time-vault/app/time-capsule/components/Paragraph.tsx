03153836b8d8cfe914e1bcc2fe6dd4bf33a08f5a
Kacp <kacperkupczynski2017@gmail.com>
2024-11-23 13:29:32 +0100

    hurra megr wowewew

--- time-vault/app/time-capsule/components/Paragraph.tsx
+++ time-vault/app/time-capsule/components/Paragraph.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect, useRef, SetStateAction } from "react";
 import styles from "../styles.module.scss";
 
-
 type ParagraphProps = {
   setMessage: React.Dispatch<SetStateAction<string[] | null>>;
   submit: boolean;
@@ -11,29 +10,14 @@ type ParagraphProps = {
 };
 
 export default function Paragraph({ id, removeParagraph }: { id: number, removeParagraph: (id: number) => void }) {
-    const [showDropdown, setShowDropdown] = useState(false);
-    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-    const [paragraphType, setParagraphType] = useState<string | null>(null);
-    const [showTextarea, setShowTextarea] = useState(false);
-    const [goals, setGoals] = useState<{ id: number; goal: string; progress: number }[]>([]);
-    const [images, setImages] = useState<string[]>([]);
-    const dropdownRef = useRef<HTMLDivElement>(null);
-    const buttonRef = useRef<HTMLButtonElement>(null);
-
-
-export default function Paragraph({
-  setMessage,
-  submit = false,
-  setSubmit,
-}: ParagraphProps) {
   const [showDropdown, setShowDropdown] = useState(false);
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
   const [paragraphType, setParagraphType] = useState<string | null>(null);
   const [showTextarea, setShowTextarea] = useState(false);
+  const [goals, setGoals] = useState<{ id: number; goal: string; progress: number }[]>([]);
+  const [images, setImages] = useState<string[]>([]);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
-  const [textAreaValue, setTextAreaValue] = useState<string | null>(null);
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleButtonClick = (event: React.MouseEvent) => {
     setCursorPosition({ x: event.clientX, y: event.clientY });
@@ -51,7 +35,6 @@ export default function Paragraph({
     }
   };
 
-
   const handleMenuItemClick = (type: string) => {
     setParagraphType(type);
     setShowDropdown(false);
@@ -60,147 +43,100 @@ export default function Paragraph({
     }
   };
 
-  const handleRemoveTextarea = () => {
-    setShowTextarea(false);
-    setParagraphType(null);
+  const handleRemoveParagraph = () => {
+    removeParagraph(id);
+  };
+
+  const addGoal = () => {
+    setGoals([...goals, { id: goals.length + 1, goal: "", progress: 0 }]);
+  };
+
+  const handleGoalChange = (id: number, field: string, value: string | number) => {
+    setGoals(goals.map(goal => goal.id === id ? { ...goal, [field]: value } : goal));
+  };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const newImages = Array.from(files).map(file => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        return new Promise<string>((resolve) => {
+          reader.onloadend = () => {
+            resolve(reader.result as string);
+          };
+        });
+      });
+      Promise.all(newImages).then(images => {
+        setImages(prevImages => [...prevImages, ...images]);
+      });
+    }
   };
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
-
-    const handleRemoveParagraph = () => {
-        removeParagraph(id);
-    };
-
-    const addGoal = () => {
-        setGoals([...goals, { id: goals.length + 1, goal: "", progress: 0 }]);
-    };
-
-    const handleGoalChange = (id: number, field: string, value: string | number) => {
-        setGoals(goals.map(goal => goal.id === id ? { ...goal, [field]: value } : goal));
-    };
-
-    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        if (files) {
-            const newImages = Array.from(files).map(file => {
-                const reader = new FileReader();
-                reader.readAsDataURL(file);
-                return new Promise<string>((resolve) => {
-                    reader.onloadend = () => {
-                        resolve(reader.result as string);
-                    };
-                });
-            });
-            Promise.all(newImages).then(images => {
-                setImages(prevImages => [...prevImages, ...images]);
-            });
-        }
-
     };
   }, []);
 
-  if (submit) {
-    if (textAreaRef.current) {
-      const taval = textAreaRef.current.value;
-      console.log(taval);
-      setMessage((p) => [...(p || []), taval]);
-    }
-    setSubmit(false);
-  }
-
-
-    return (
-        <div className={styles.main_add_paragraph}>
-            {paragraphType === "Text" && (
-                <div className={styles.paragraph}>
-                    <textarea
-            className={styles.textarea}
-            placeholder="Write your message here..."
-            ref={textAreaRef}
-          ></textarea>
-          <button
-            onClick={handleRemoveTextarea}
-            className={styles.remove_button}
-          >
-            Remove
-          </button>
-                </div>
-            )}
-            {paragraphType === "Question" && (
-                <div className={styles.paragraph}>
-                    <input type="text" className={styles.input} placeholder="Enter your question here..." />
-                    <input type="text" className={styles.input} placeholder="Enter your answer here..." />
-                    <button onClick={handleRemoveParagraph} className={styles.remove_button}>Remove</button>
-                </div>
-            )}
-            {paragraphType === "Goals" && (
-                <div className={styles.paragraph}>
-                    <div className={styles.goals_list}>
-                        {goals.map(goal => (
-                            <div key={goal.id} className={styles.goal}>
-                                <input
-                                    type="text"
-                                    className={styles.input}
-                                    placeholder="Enter your goal here..."
-                                    value={goal.goal}
-                                    onChange={(e) => handleGoalChange(goal.id, "goal", e.target.value)}
-                                />
-                                <input
-                                    type="range"
-                                    className={styles.slider}
-                                    value={goal.progress}
-                                    onChange={(e) => handleGoalChange(goal.id, "progress", Number(e.target.value))}
-                                />
-                                <span className={styles.slider_value}>{goal.progress}%</span>
-                            </div>
-                        ))}
-                    </div>
-                    <button onClick={addGoal} className={styles.add_goal_button}>Add Goal</button>
-                    <button onClick={handleRemoveParagraph} className={styles.remove_button}>Remove</button>
-                </div>
-            )}
-            {paragraphType === "Image" && (
-                <div className={styles.paragraph}>
-                    <input type="file" className={styles.input_file} accept="image/*" multiple onChange={handleImageChange} />
-                    <div className={styles.image_preview_container}>
-                        {images.map((image, index) => (
-                            <div key={index} className={styles.image_preview}>
-                                <img src={image} alt={`Preview ${index}`} />
-                            </div>
-                        ))}
-                    </div>
-                    <button onClick={handleRemoveParagraph} className={styles.remove_button}>Remove</button>
-                </div>
-            )}
-            {!paragraphType && (
-                <>
-                    <button ref={buttonRef} onClick={handleButtonClick} className={styles.add_paragraph_button}>Add paragraph</button>
-                    {showDropdown && (
-                        <div
-                            ref={dropdownRef}
-                            className={styles.dropdown_menu}
-                            style={{ top: cursorPosition.y, left: cursorPosition.x }}
-                        >
-                            <ul>
-                                <li onClick={() => handleMenuItemClick("Text")}>Text</li>
-                                <li onClick={() => handleMenuItemClick("Question")}>Question</li>
-                                <li onClick={() => handleMenuItemClick("Goals")}>Goals</li>
-                                <li onClick={() => handleMenuItemClick("Image")}>Image</li>
-                            </ul>
-                        </div>
-                    )}
-                </>
-            )}
-
+  return (
+    <div className={styles.main_add_paragraph}>
+      {paragraphType === "Text" && (
+        <div className={styles.paragraph}>
+          <textarea className={styles.textarea} placeholder="Write your message here..."></textarea>
+          <button onClick={handleRemoveParagraph} className={styles.remove_button}>Remove</button>
         </div>
-      ) : (
+      )}
+      {paragraphType === "Question" && (
+        <div className={styles.paragraph}>
+          <input type="text" className={styles.input} placeholder="Enter your question here..." />
+          <input type="text" className={styles.input} placeholder="Enter your answer here..." />
+          <button onClick={handleRemoveParagraph} className={styles.remove_button}>Remove</button>
+        </div>
+      )}
+      {paragraphType === "Goals" && (
+        <div className={styles.paragraph}>
+          {goals.map(goal => (
+            <div key={goal.id} className={styles.goal}>
+              <input
+                type="text"
+                className={styles.input}
+                placeholder="Enter your goal here..."
+                value={goal.goal}
+                onChange={(e) => handleGoalChange(goal.id, "goal", e.target.value)}
+              />
+              <div className={styles.slider_container}>
+                <input
+                  type="range"
+                  className={styles.slider}
+                  value={goal.progress}
+                  onChange={(e) => handleGoalChange(goal.id, "progress", Number(e.target.value))}
+                />
+                <span className={styles.slider_value}>{goal.progress}%</span>
+              </div>
+            </div>
+          ))}
+          <button onClick={addGoal} className={styles.add_goal_button}>Add Goal</button>
+          <button onClick={handleRemoveParagraph} className={styles.remove_button}>Remove</button>
+        </div>
+      )}
+      {paragraphType === "Image" && (
+        <div className={styles.paragraph}>
+          <input type="file" className={styles.input_file} accept="image/*" multiple onChange={handleImageChange} />
+          <div className={styles.image_preview_container}>
+            {images.map((image, index) => (
+              <div key={index} className={styles.image_preview}>
+                <img src={image} alt={`Preview ${index}`} />
+              </div>
+            ))}
+          </div>
+          <button onClick={handleRemoveParagraph} className={styles.remove_button}>Remove</button>
+        </div>
+      )}
+      {!paragraphType && (
         <>
-          <button ref={buttonRef} onClick={handleButtonClick}>
-            Add paragraph
-          </button>
+          <button ref={buttonRef} onClick={handleButtonClick} className={styles.add_paragraph_button}>Add paragraph</button>
           {showDropdown && (
             <div
               ref={dropdownRef}
@@ -209,9 +145,7 @@ export default function Paragraph({
             >
               <ul>
                 <li onClick={() => handleMenuItemClick("Text")}>Text</li>
-                <li onClick={() => handleMenuItemClick("Question")}>
-                  Question
-                </li>
+                <li onClick={() => handleMenuItemClick("Question")}>Question</li>
                 <li onClick={() => handleMenuItemClick("Goals")}>Goals</li>
                 <li onClick={() => handleMenuItemClick("Image")}>Image</li>
               </ul>
@@ -222,4 +156,3 @@ export default function Paragraph({
     </div>
   );
 }
-
