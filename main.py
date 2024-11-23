7a90e2f64ae7a2ef75771460ade024cd5a63921d
Jakub <jakubkulik20@gmail.com>
2024-11-23 10:03:43 +0100

    fix

--- main.py
+++ main.py
@@ -119,4 +119,4 @@ async def hash_password(password):
     return hash_object.hexdigest()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
